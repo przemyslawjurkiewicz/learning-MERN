ce9bed195152ca8f6790eaaa09ec5f5f353f1a4b
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-05-02 11:29:20 +0200

    Fix thumbUp and thumbDown on server site

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -17,4 +17,10 @@ router.route('/posts/:cuid').delete(PostController.deletePost);
 // Update a post by cuid
 router.route('/posts/:cuid').put(PostController.editPost);
 
+// ThumbUp a post by cuid
+router.route('/posts/:cuid').put(PostController.thumbUp);
+
+// ThumbDow  a post by cuid
+router.route('/posts/:cuid').put(PostController.thumbDown);
+
 export default router;
